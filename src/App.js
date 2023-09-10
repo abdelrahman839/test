31fef44a703ca6a7e2ae448da655c83c979c937c
abdelrahman839 <asdddghjh@gmail.com>
2023-09-10 17:30:19 +0300

    test

--- src/App.js
+++ src/App.js
@@ -5,8 +5,17 @@ import { Manager } from "socket.io-client";
 function App() {
   const token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoidXNlciIsInVzZXJJZCI6IjY0YzI0Y2ZlYzljYjJkYmM0OTI4NzIyOSIsImFjdGl2YXRlZCI6dHJ1ZSwiZW1haWwiOiJhYmRlbHJhaG1hbmFiZGFsbGFoOTgwMEBnbWFpbC5jb20iLCJ1c2VybmFtZSI6ImFiZGVscmFobWFuMjMiLCJpYXQiOjE2OTA0NTUyOTR9.Jz6RSBoNz9XDlaeFqj0I_Cbzq77FSmnHUBw0bmPF7sE";
 
-  const socket = io(`https://api.halajary.ae?token=${token}`);
-  console.log(socket)
+  const socket = io(`https://api.halajary.ae?token=${token}`, {
+    transports: ["websocket"],
+    autoConnect: false,
+    rejectUnauthorized: false
+  });
+  socket.connect()
+  socket.on('error', err => console.log(`error: $err`));
+  socket.on('connect', err => {
+    console.log(`connect`)
+    console.log(socket)
+  });
 
   return (
     <div className="App">
